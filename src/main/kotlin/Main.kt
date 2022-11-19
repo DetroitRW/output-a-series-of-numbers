2cc22be94ff9cf0bed396a8549ea60512e22af33
Ilya Brant <detroitib31@gmail.com>
2022-11-20 00:48:45 +0600

    Написал код для ввода данных пользователем

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,3 +3,11 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
+    print("Введите минимальное натуральное число: ")
+    var a = reader.nextInt()
+
+    print("Введите максимальное натуральное число: ")
+    val b = reader.nextInt()
+
+    print("Введите шаг: ")
+    val c = reader.nextInt()
